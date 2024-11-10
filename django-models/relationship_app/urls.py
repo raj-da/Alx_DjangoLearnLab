7d8851092d936c09e88407970666bc124d5d9ec7
raj-da <rajafgudeta@gmail.com>
2024-11-10 13:57:37 +0300

    Added authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import list_books, views
+from .views import list_books, views, register, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
-    path('library/<int:pk', views.LibraryDetailView.as_view(), name='library_detail')
+    path('library/<int:pk', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_View(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
 
 def book_list(request):
     books = Book.objects.all()
@@ -19,3 +24,24 @@ class LibraryDetailView(DetailView):
         Library = self.get_object()
         context['books'] = Library.books.all()
         return context
+    
+
+# Registration view
+def register(request):
+    if request.method == "post":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
