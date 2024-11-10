32508bae5514bc52f9514d03cb0ea3f8c985ce15
raj-da <rajafgudeta@gmail.com>
2024-11-10 20:49:11 +0300

    update

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'), 
-    path('add/', views.add_book, name='add-book'),
-    path('edit/<int:pk>/', views.edit_book, name='edit-book'),
+    path('add_book/', views.add_book, name='add-book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit-book'),
     path('delete/<int:pk>/', views.delete_book, name='delete-book'),
     path('list/', views.book_list, name='book-list'),
 ]
