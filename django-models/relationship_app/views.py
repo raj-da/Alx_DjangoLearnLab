27900b1fad5d44a641caa4efb1c312662860c04c
raj-da <rajafgudeta@gmail.com>
2024-11-10 14:21:51 +0300

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -60,14 +60,14 @@ def is_member(user):
 @user_passes_test(is_admin)
 @login_required
 def admin_view(request):
-    return render(request, 'admin_view.html', {})
+    return render(request, 'relationship_app/admin_view.html', {})
 
 @user_passes_test(is_librarian)
 @login_required
 def librarian_view(request):
-    return render(request, 'librarian_view.html', {})
+    return render(request, 'relationship_app/librarian_view.html', {})
 
 @user_passes_test(is_member)
 @login_required
 def member_view(request):
-    return render(request, 'member_view.html', {})
+    return render(request, 'relationship_app/member_view.html', {})
