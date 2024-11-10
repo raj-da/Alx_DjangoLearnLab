9b0359cbb0e29aa7f8b2a34d9286fb8190d9254d
raj-da <rajafgudeta@gmail.com>
2024-11-10 13:37:53 +0300

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from models import Library, Book
+from .models import Library, Book
 
 def book_list(request):
     books = Book.objects.all()
