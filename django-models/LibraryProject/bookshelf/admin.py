fb6c5b8181ef709ad69c938a7092ee2ecc3d145e
raj-da <rajafgudeta@gmail.com>
2024-11-10 13:13:03 +0300

    deleted the other files

--- django-models/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_year")
-    list_filter = ("title", "author")
-    search_fields = ("title", "author")
-
-admin.site.register(Book, BookAdmin)
--- django-models/LibraryProject/relationship_app/query_samples.py
+++
@@ -1,7 +0,0 @@
-from models import Book, Author, Library, Librarian
-
-book = Book.objects.filter(author='Rajaf')
-
-library = Library.objects.all()
-
-librarian = Librarian.objects.filter(library='ox')
