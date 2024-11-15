0e8a3ee7fdb36b510b5ec8d2ceca386f5bc120ec
raj-da <rajafgudeta@gmail.com>
2024-11-15 22:24:48 +0300

    4. Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'), 
+    path('member/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add-book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit-book'),
+    path('delete/<int:pk>/', views.delete_book, name='delete-book'), 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Library, Book, UserProfile
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 
 
 def book_list(request):
@@ -67,3 +67,16 @@ def librarian_view(request):
 @login_required
 def member_view(request):
     return render(request, 'relationship_app/member_view.html', {})
+
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    return render(request, 'relationship_app/add_book.html', {})
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    return render(request, 'relationship_app/edit_book.html', {})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    return render(request, 'relationship_app/delete_book.html', {})
