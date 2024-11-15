b4a267fc03a66bc922cdbf245e6f7edd63a934b4
raj-da <rajafgudeta@gmail.com>
2024-11-15 15:11:24 +0300

    2. Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import book_list, LibraryDetailView
+from .views import book_list, LibraryDetailView, CustomLoginView, CustomLogoutView, register
 
 
 urlpatterns = [
     path('books/', book_list, name='book_list'),
     path('library/<int:pk', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Library, Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 def book_list(request):
@@ -21,3 +23,19 @@ class LibraryDetailView(DetailView):
         Library = self.get_object()
         context['books'] = Library.books.all()
         return context
+    
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+        
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
