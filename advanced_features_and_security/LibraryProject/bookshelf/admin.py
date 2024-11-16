1b02c3153a481ca7c47b52fc6ed6fd1e4be45383
raj-da <rajafgudeta@gmail.com>
2024-11-16 21:15:25 +0300

    Update

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
 from .models import NewUser
 
 class BookAdmin(admin.ModelAdmin):
@@ -7,5 +8,14 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("title", "author")
     search_fields = ("title", "author")
 
+class NewUserAdmin(UserAdmin):
+    model = NewUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
 admin.site.register(Book, BookAdmin)
-admin.site.register(NewUser)
+admin.site.register(NewUser, NewUserAdmin)
